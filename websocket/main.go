63e16f3df3abc9eff48c1aafe10e8f0046c0ec4e
小小志 <1601474335@qq.com>
2023-07-27 18:06:17 +0800

    完善发送消息

--- websocket/main.go
+++ websocket/main.go
@@ -12,15 +12,33 @@ import (
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	// websocket 升级允许跨域
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
 }
 
 func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	// 可以通过参数传输token来验证用户
+	fmt.Println("token为: ", r.FormValue("token"))
+	token := r.FormValue("token")
+	// checkToken方法，来验证连接是否安全 token可以转为是用户。
+	if token == "" {
+		//data := map[string]string{"err_reason": "unauthorized"}
+		//errData, err := json.Marshal(data)
+		//if err != nil {
+		//	w.WriteHeader(401)
+		//	w.Write(errData)
+		//}
+		return
+	}
 	// 升级 HTTP 连接为 WebSocket 连接
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
 		return
 	}
+	// 服务端逻辑代码
 	go func() {
 		fmt.Println("开始监听消息")
 		for {
@@ -41,7 +59,9 @@ func handleWebSocket(w http.ResponseWriter, r *http.Request) {
 		for {
 			time.Sleep(time.Second)
 			// 回复消息给客户端
-			err = conn.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("message :%d", count)))
+			message := fmt.Sprintf("message :%d", count)
+			log.Println("发送了: " + message)
+			err = conn.WriteMessage(websocket.TextMessage, []byte(message))
 			if err != nil {
 				log.Println(err)
 				return
@@ -59,5 +79,5 @@ func main() {
 	handler := http.StripPrefix(prefix, fileServer)
 	http.Handle(prefix, handler)
 	log.Println("http://localhost:8765/static/")
-	log.Fatal(http.ListenAndServe("localhost:8765", nil))
+	log.Fatal(http.ListenAndServe(":8765", nil))
 }
